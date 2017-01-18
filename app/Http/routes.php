785e1a5eaa08418c79a721c740e38e643ec775a4
francisco Hernandez <francisco20990@gmail.com>
2017-01-17 22:23:50 -0400

    Plantilla + model(equipos,usuarios) + fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('usuarios', function () {
+    return view('usuarios/index');
+});
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,72 @@
+<?
+if(isset($_SESSION['id_user'])){
+  session_destroy();
+}
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Informatica</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{asset('css/_all-skins.min.css')}}">
+    <link rel="apple-touch-icon" href="{{asset('img/apple-touch-icon.png')}}">
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
- 
-                <div class="title">Laravel 5</div>
+  </head>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="../../index2.html"><b>INGRESO AL SISTEMA</b></a>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">Ingrese sus datos de Acceso</p>
+        <form id="login-form" action="usuarios" method="post">
+          <input hidden name="action" value="login">
+          <div class="form-group has-feedback">
+            <input class="form-control" type="text" name="cedula" placeholder="Cedula">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input class="form-control" type="password" name="password" placeholder="Password">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="form-group">
+            <div class="progress" style="display:none">
+              <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">
+              </div>
             </div>
-        </div>
-    </body>
+          </div>
+          <div class="alert alert-danger" role="alert" style="display:none">
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;
+            <span id="msj">Datos incorrectos</span>
+          </div>
+          <div class="row">
+            <div class="col-xs-8">
+              
+            </div><!-- /.col -->
+            <div class="col-xs-4">
+              <a id="enviar"  class="btn btn-primary btn-block btn-flat" href="usuarios">Ingresar</a>
+            </div><!-- /.col -->
+          </div>
+        </form>
+
+        
+    
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+
+   
+
+  </body>
 </html>
